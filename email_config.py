310e45b7f55ada7d427b775ec2bae876c19114b4
Pravinsinh Mahida <48326889+pravinmahida@users.noreply.github.com>
2019-08-29 23:31:11 +0530

    reverted changes.

--- email_config.py
+++ email_config.py
@@ -1,6 +1,6 @@
 
 def Config():
-	gmail_user = 'inquiry.pravin@gmal.com' # Gmail Username
-	gmail_pwd = 'mca@aiis' #Gmail Password
+	gmail_user = 'gmail@gmal.com' # Gmail Username
+	gmail_pwd = 'password' #Gmail Password
 	gmail_config = (gmail_user, gmail_pwd)
 	return gmail_config
